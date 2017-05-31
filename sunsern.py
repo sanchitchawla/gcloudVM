fe4b9ec765698aafc62782b4b16381889ceea41d
Sanchit Chawla <sanchit_sid@hotmail.com>
2017-06-01 00:18:48 +0700

    pushing for the night

--- sunsern.py
+++ sunsern.py
@@ -3,21 +3,6 @@ from gym import wrappers
 import numpy as np
 import mdptoolbox, mdptoolbox.example
 
-# bins = [[-0.11781816, -0.03565303, 0.00311038, 0.04424798, 0.14451232],
-#        [ -9.38700600e-01, -3.75870575e-01, 9.40468447e-04, 3.52090368e-01, 9.94038331e-01],
-#        [-0.17349113, -0.05851606, 0.00506065, 0.05917423, 0.18454919],
-#        [-1.4517017, -0.53794407, -0.00709455, 0.59268654, 1.405515]]
-
-# bins = [[-0.03187129,  0.00126071,  0.03691676],
-#         [-0.21083134,  0.02977822,  0.35517135],
-#         [-0.05921202, -0.01302745,  0.02886646],
-#         [-0.56682612, -0.15239829,  0.29063866]]
-
-# bins = [[-0.00595004,  0.02962881],
-#         [-0.15417628,  0.18324071],
-#         [-0.03491213,  0.0106769 ],
-#         [-0.29086968,  0.19455562]]
-
 bins = [[-0.77038577, -0.01766937],
 [-0.78736876, -0.01698298],
 [-0.80357355, -0.0162048 ],
@@ -32,6 +17,7 @@ def normalize(m):
     for j in range(m.shape[0]):
         for i in range(m.shape[1]):
             m[j,i,:] = m[j,i,:] / m[j,i,:].sum()
+    return m
 
 def obs2state(obs):
     ans = 0;
@@ -67,29 +53,49 @@ for i_episode in range(1000):
         #print from_state, to_state, "STATES"
         r += reward
         if done:
-            print R
-            # only penalize if early terminate
+            #print R
             if t == 199:
                 R[to_state] = -1000.0
+            else:
+                R[to_state] = 1000
             print("{}:Episode finished after {} timesteps".format(i_episode, t+1))
             perf.append(t+1)
             break
 
 
-        if t < 160:
+        if t < 170:
             # only retrain if below target
             P[action,from_state,to_state] += 0.50
             #print P, "P"
-            normalize(P)
+            P = normalize(P)
             H = min(200-t,200)
-            #mdp = mdptoolbox.mdp.FiniteHorizon(P,R,0.9,H)
+            mdp = mdptoolbox.mdp.FiniteHorizon(P,R,0.9,H)
             #mdp = mdptoolbox.mdp.QLearning(P,R,0.9)
-            mdp = mdptoolbox.mdp.PolicyIterationModified(P, R, 0.99, epsilon=0.001, max_iter=300)
+            #mdp = mdptoolbox.mdp.PolicyIterationModified(P, R, 0.99, epsilon=0.001, max_iter=300)
+            mdp.run()
+        if t == 198 and not done:
+            # only retrain if below target
+            P[action,from_state,to_state] *= 0.000001
+            P = normalize(P)
+            H = min(200-t,200)
+            mdp = mdptoolbox.mdp.FiniteHorizon(P,R,0.9,H)
+            #mdp = mdptoolbox.mdp.QLearning(P,R,0.9)
+            #mdp = mdptoolbox.mdp.PolicyIterationModified(P, R, 0.99, epsilon=0.001, max_iter=300)
             mdp.run()
 
-
-        action = int(mdp.policy[to_state])
+        if t < 199 and done:
+            "YES IT SOLVED ONCE"
+            P[action,from_state,to_state] += 100
+            print P, "P"
+            P = normalize(P)
+            H = min(200-t,200)
+            mdp = mdptoolbox.mdp.FiniteHorizon(P,R,0.9,H)
+            #mdp = mdptoolbox.mdp.QLearning(P,R,0.9)
+            #mdp = mdptoolbox.mdp.PolicyIterationModified(P, R, 0.99, epsilon=0.001, max_iter=300)
+            mdp.run()
+            break
+        #action = int(mdp.policy[to_state])
         #action = env.action_space.sample()
-        #action = mdp.policy[to_state,0]
+        action = mdp.policy[to_state,0]
     reward_sum.append(r)
     #print "Reward for episode {} is {}".format(i_episode, reward_sum[-1])
