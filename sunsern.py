c845e67c9545e6b7fde9836ec9886f29fac80082
Sanchit Chawla <sanchit_sid@hotmail.com>
2017-05-31 22:25:45 +0700

    pushing to compute engine

--- sunsern.py
+++ sunsern.py
@@ -13,13 +13,18 @@ import mdptoolbox, mdptoolbox.example
 #         [-0.05921202, -0.01302745,  0.02886646],
 #         [-0.56682612, -0.15239829,  0.29063866]]
 
-bins = [[-0.00595004,  0.02962881],
-        [-0.15417628,  0.18324071],
-        [-0.03491213,  0.0106769 ],
-        [-0.29086968,  0.19455562]]
+# bins = [[-0.00595004,  0.02962881],
+#         [-0.15417628,  0.18324071],
+#         [-0.03491213,  0.0106769 ],
+#         [-0.29086968,  0.19455562]]
+
+bins = [[-0.77038577, -0.01766937],
+[-0.78736876, -0.01698298],
+[-0.80357355, -0.0162048 ],
+[-0.84076519, -0.01143178]]
 
 num_states = (len(bins[0])+1)**len(bins)
-P,_ = mdptoolbox.example.rand(num_states, 2)
+P,_ = mdptoolbox.example.rand(num_states, 3)
 R = np.ones((num_states,))
 reward_sum = []
 
@@ -38,50 +43,53 @@ def obs2state(obs):
 all_obs = []
 perf = []
 
-#name = 'MountainCar-v0'
-name = 'CartPole-v0'
+name = 'MountainCar-v0'
+#name = 'CartPole-v0'
 env = gym.make(name)
 #env = wrappers.Monitor(env, '/tmp/cartpole-experiment-3')
 
-for i_episode in range(200):
+for i_episode in range(1000):
     observation = env.reset()
     action = env.action_space.sample()
     r = 0
-    for t in range(250):
+    for t in range(200):
 
-        #env.render()
+        env.render()
 
         from_state = obs2state(observation)
-        print observation
+        #print observation
 
         observation, reward, done, info = env.step(action)
-
+        #print action
         all_obs.append(observation)
 
         to_state = obs2state(observation)
         #print from_state, to_state, "STATES"
         r += reward
-        reward_sum.append(r)
         if done:
+            print R
             # only penalize if early terminate
-            if t < 195:
+            if t == 199:
                 R[to_state] = -1000.0
             print("{}:Episode finished after {} timesteps".format(i_episode, t+1))
             perf.append(t+1)
             break
 
 
-        if t < 195:
+        if t < 160:
             # only retrain if below target
             P[action,from_state,to_state] += 0.50
+            #print P, "P"
             normalize(P)
             H = min(200-t,200)
-            mdp = mdptoolbox.mdp.FiniteHorizon(P,R,0.9,H)
-            #mdp = mdptoolbox.mdp.PolicyIterationModified(P, R, 0.99, epsilon=0.001, max_iter=300)
+            #mdp = mdptoolbox.mdp.FiniteHorizon(P,R,0.9,H)
+            #mdp = mdptoolbox.mdp.QLearning(P,R,0.9)
+            mdp = mdptoolbox.mdp.PolicyIterationModified(P, R, 0.99, epsilon=0.001, max_iter=300)
             mdp.run()
 
 
-        #action = mdp.policy[to_state]
+        action = int(mdp.policy[to_state])
         #action = env.action_space.sample()
-        action = mdp.policy[to_state,0]
+        #action = mdp.policy[to_state,0]
+    reward_sum.append(r)
     #print "Reward for episode {} is {}".format(i_episode, reward_sum[-1])
